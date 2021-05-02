6c4af47ea8623219686c0228185ef9068db2b9e3
DESKTOP-5BSR2KC\YM <ymiru0324@naver.com>
2021-05-02 20:23:29 +0900

    수정 사항 추가 : ClassVo.java, ClassDao.java, ClassMapper.xml, RoomVo.java, RoomDao.java, RoomMapper.xml 수정 : context-database.xml, ClassController.java, registration.jsp

---
+++ amp/src/main/java/com/jj/vo/ClassVo.java
@@ -0,0 +1,65 @@
+package com.jj.vo;
+
+public class ClassVo {
+	
+	int idx, room;
+	String name,time, s_date, e_date, teacher, price, limited;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public int getRoom() {
+		return room;
+	}
+	public void setRoom(int room) {
+		this.room = room;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getS_date() {
+		return s_date;
+	}
+	public void setS_date(String s_date) {
+		this.s_date = s_date;
+	}
+	public String getE_date() {
+		return e_date;
+	}
+	public void setE_date(String e_date) {
+		this.e_date = e_date;
+	}
+	public String getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getLimited() {
+		return limited;
+	}
+	public void setLimited(String limited) {
+		this.limited = limited;
+	}
+	
+	
+
+}
